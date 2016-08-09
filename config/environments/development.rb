5efbc7a19dc5429f909ed2267b92b62a5e82941c
Breen <breenho@gmail.com>
2016-08-09 12:36:03 +0530

    mailer configurations and confirmable added on registration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,20 @@ Garfield::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Email
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com', 
+    port: '587',
+    enable_starttls_auto: true,
+    user_name: 'qwertybreenho@gmail.com',
+    password: 'MockingJay',
+    authentication: :plain,
+    domain: 'localhost:3000'
+  }
 end
